78dda3317f2c37151eb2dd888e3d90b30b5aaf40
Harshita C Prabhu B <53487702+harshitaprabhu99@users.noreply.github.com>
2020-02-05 16:34:06 +0530

    Add files via upload

---
+++ google.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC >
+<head>
+<title>C-Handbook</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
+<script type="text/javascript" src="layout/scripts/jquery.min.js"></script>
+<script type="text/javascript" src="layout/scripts/jquery.jcarousel.pack.js"></script>
+<script type="text/javascript" src="layout/scripts/jquery.jcarousel.setup.js"></script>
+<style type="text/css">
+  .last{
+
+  }
+  img{
+    width: 60px;
+  }
+</style>
+</head>
+<body id="top">
+  <body bgsrc="">
+<div class="wrapper col1">
+  <div id="header">
+    <div id="logo">
+      <h1><a href="index.html">C-Handbook</a></h1>
+    </div>
+    <div id="topnav">
+      <ul>
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="SECTORS">About</a>
+          <ul>
+            <li><a href="micro.php">IT SECTOR</a></li>
+            <li><a href="next1.php">BANKING SECTOR></a></li>
+            <li><a href="air.php">GOVERNMENT SECTOR</a></li>
+            <li><a href="full-width.php">Details</a></li>
+          </ul>
+          <li class="active"><a href="F:\html\HTML Images_files\logins.html">LOGIN</a></li>
+          <ul>
+          <li><a href="myform.php">SIGN UP</a></li>
+        </ul>
+        </li>
+      </ul>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<div class="wrapper col2">
+  <div id="featured_slide">
+    <div id="featured_content">
+      <ul>
+        <li><img src="google.JPG" alt="" />
+          <div class="floater">
+            <h2>GOOGLE</h2>
+            <p>Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple and Facebook.</p>
+            <p class="readmore"><a href="googlereal.php">Life at Google&raquo;</a></p>
+          </div>
+        </li>
+        <li><img src="infosys.JPG" alt="" />
+          <div class="floater">
+            <h2>INFOSYS</h2>
+            <p>Infosys Limited is an Indian multinational corporation that provides business consulting, information technology and outsourcing services. It has its headquarters in Bangalore, Karnataka, India.</p>
+            <p class="readmore"><a href="infosys.php">Infosys the Lifestyle &raquo;</a></p>
+                    </div>
+        </li>
+        <li><img src="333.JPG" alt="" />
+          <div class="floater">
+            <h2>MICROSOFT</h2>
+            <p>Microsoft Corporation (MS) is an American multinational technology company with headquarters in Redmond, Washington. It develops, manufactures, licenses, supports and sells computer software, consumer electronics, personal computers, and related services. Its best known software products are the Microsoft Windows line of operating systems, the Microsoft Office suite, and the Internet Explorer and Edge web browsers.</p>
+              <p class="readmore"><a href="micro.php">Continue Reading &raquo;</a></p>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <a href="javascript:void(0);" id="featured-item-prev"><img src="layout/images/prev.png" alt="" /></a> <a href="javascript:void(0);" id="featured-item-next"><img src="layout/images/next.png" alt="" /></a> </div>
+</div>
+<div class="wrapper col3">
+  <div id="container">
+    <div class="homepage">
+      <ul>
+        <li>
+          <h2><img src="air1.jpg" alt="" />Air India </h2>
+          <p>Air India is the flag carrier airline of India headquartered at New Delhi.[8] It is owned by Air India Limited, a government-owned enterprise, and operates a fleet of Airbus and Boeing aircraft serving 94 domestic and international destinations. The airline has its hub at Indira Gandhi International Airport, New Delhi, alongside several focus cities across India. </p>
+          <p class="readmore"><a href="air.php">Continue Reading &raquo;</a></p>
+        </li>
+        <li>
+          <h2><img src="micro3.jpg" alt="" />Microsoft</h2>
+          <p>Microsoft Corporation (MS) is an American multinational technology company with headquarters in Redmond, Washington. It develops, manufactures, licenses, supports and sells computer software, consumer electronics, personal computers, and related services. Its best known software products are the Microsoft Windows line of operating systems, the Microsoft Office suite, and the Internet Explorer and Edge web browsers. </p>
+          <p class="readmore"><a href="micro.php">Continue Reading &raquo;</a></p>
+        </li>
+        <li class="last">
+          <h2><img src="download.png"  alt="" />HDFC Bank Limited</h2>
+          <p>HDFC Bank is India's largest private sector lender by assets.Hdfc Bank Ltd is an Indian banking and finance services company haedquartered in Mumbai,Maharashtra.It has 88,253 permanent employees as of 31st March 2018 and has a presence in Bahrain, Hong Kong and Dubai.</p>
+          <p class="readmore"><a href="next.php">Continue Reading &raquo;</a></p>
+        </li>
+      </ul>
+      <br class="clear" />
+    </div>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+
+<!-- ####################################################################################################### -->
+<div class="wrapper col5">
+	<font color="White"><font size="3"><marquee>C-HANDBOOK for more info contact:775685454</marquee></font>
+    <br class="clear" />
+  </div>
+</div>
+</body>
+</html>
---
+++ next1.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     <style type="text/css">
+     	
+     	h1
+     	{
+     		color:#fb1b08;
+     		font-size: 500%;
+     		background-color: #0c186a;
+     	}
+     	body{
+     		background-color: #0000 ;
+     	}
+     	.card{
+     		box-shadow: 3px 3px 13px 6px #e3d5d5;
+     		margin-left: 04px;
+     	}
+     </style>
+    <title>C-HandBook</title>
+  </head>
+  <body>
+   
+
+     <div class="text-center">
+     	<h1>HDFC Bank</h1>
+    <img class="img-fluid" src="HDFC_Bank_logo.jpg" width=300px height=300px >
+     </div>
+
+    
+
+
+<div class="card-group">
+  <div class="card">
+    <img src="moni.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-titleHousing Development Finance Corporation, an Indian mortgage company.
+HDFC Bank, an Indian financial services company.
+HDFC Life, a joint venture of Housing Development Finance Corporation and Standard Life plc.
+Housing Development Fund Corporation, a special type of limited equity housing cooperative in New York City.">HDFC may refer to</h5>
+      <p class="card-text">Housing Development Finance Corporation, an Indian mortgage company.<br>
+HDFC Bank, an Indian financial services company.<br>
+HDFC Life, a joint venture of Housing Development Finance Corporation and Standard Life plc.<br>
+Housing Development Fund Corporation, a special type of limited equity housing cooperative in New York City.</p>
+      <p class="card-text"><small class="text-muted"></small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="rupees.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Acquisitions</h5>
+      <p class="card-text">HDFC Bank merged with Times Bank in February 2000. This was the first merger of two private banks in the New Generation private sector banks category.In 2008, Centurion Bank was acquired by HDFC Bank. HDFC Bank Board approved the acquisition of CBoP for 95.1 billion INR in one of the largest mergers in the financial sector in India.</p>
+      <button><a href="index.html">www.hdfcbank.com</a></button>
+      <p class="card-text"><small class="text-muted"></small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="rupaya.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Awards and Recognition</h5>
+      <p class="card-text">2019<br>
+Bank of the Year and Best Large Bank, Business Today – Money Today Financial Awards 2019
+Best Bank in India 2019, by Global magazine FinanceAsia.
+Ranked 60th in 2019 BrandZ Top 100 Most Valuable Global Brands HDFC Bank was featured BrandZ Top 100 Most Valuable Global Brands 2019 for the 5th consecutive year. The Bank's brand value has gone up from $20.87 billion in 2018 to $22.70 billion in 2019.
+Best Large Bank & Fastest Growing Large Bank in 2019, by Business World Magna Awards
+2016.Best Banking Performer, India in 2016 by Global Brands Magazine Award.</p>
+      <p class="card-text"><small class="text-muted"></small></p>
+    </div>
+  </div>
+</div>
+
+
+     <p>
+     	
+
+
+
+
+<h6>HDFC Bank Limited (Housing Development Finance Corporation) is an Indian banking and financial services company headquartered in Mumbai, Maharashtra. It has 88,253 permanent employees as of 31 March 2018 and has a presence in Bahrain, Hong Kong and Dubai.HDFC Bank is India’s largest private sector lender by assets. It is the largest bank in India by market capitalization as of February 2016. It was ranked 69th in 2016 BrandZ Top 100 Most Valuable Global Brands.</h6><br>
+<h2 style="background-color:rgb(102, 153, 255);">History</h2>
+<h6>In 1994 HDFC Bank was incorporated, with its registered office in Mumbai, India. Its first corporate office and a full service branch at Sandoz House, Worli were inaugurated by the then Union Finance Minister, Manmohan Singh.
+As of October 9, 2018, the bank's distributions network was at 4,805 branches and 12,260 ATMs across 2,657 cities and towns. The bank also installed 4.30 Lacs POS terminals and issued 235.7 Lacs debit cards and 85.4 Lacs credit card in FY 2017.[6]</h6><br>
+
+<h2 style="background-color:rgb(255, 99, 71);">Products and Services</h2>
+<h2 style="background-color:#ff6347;"></h2>
+<h6>Accounts & Deposits
+Experience banking of the future with a bank account from HDFC Bank. A world of smart features, multiple banking channels and prompt customer service awaits you.
+
+
+<br>
+Loans
+Whatever be your need, we have a loan for you. Choose HDFC Bank for quick check on eligibility, competitive interest rates, flexible terms, minimum paperwork and fast disbursal.</h6>
+</h2>
+</h2>
+</p>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
